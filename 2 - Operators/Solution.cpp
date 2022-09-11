22fb58874357d69cca44b5ca6a2ac1abf040cdaa
Aditya Seth <adityaseth.cse@gmail.com>
2022-09-11 22:18:10 +0530

    Add files via upload

---
+++ 2 - Operators/Solution.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string tmp;
+
+    getline(cin, tmp);
+    double mealCost = stod(tmp);
+    //Aditya Seth
+   
+
+    getline(cin, tmp);
+    int tipPercent = stoi(tmp);
+
+    getline(cin, tmp);
+    int taxPercent = stoi(tmp);
+
+    double tip = tipPercent * mealCost / 100;
+    double tax = taxPercent * mealCost / 100;
+
+    int totalCost = (int) round(tip + tax + mealCost);
+    cout<<totalCost;
+
+    return 0;
+}
