432955f99261562fb79b89259231bc6f62f701b4
Aditya Seth <adityaseth.cse@gmail.com>
2022-09-11 22:21:26 +0530

    Add files via upload

---
+++ 3 - Intro to Conditional Statements/Solution.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Aditya Seth
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    if(n%2==1 || (n%2==0 && n>=6 && n<=20))
+    printf("Weird\n");
+    else if(n%2==0 && ( (n>=2 && n<=5) || n>20))
+    printf("Not Weird\n");
+
+}
---
+++ 3 - Intro to Conditional Statements/Solution.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+if N % 2 != 0:
+    print("Weird")
+else:
+    if N <= 5:
+        print("Not Weird")
+    elif N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+#Aditya Seth
