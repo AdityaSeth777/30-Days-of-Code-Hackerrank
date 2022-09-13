76f230fcf393a1ee544886632d633971fd6b1e5a
Aditya Seth <adityaseth.cse@gmail.com>
2022-09-13 22:06:12 +0530

    Add files via upload

---
+++ 15 - Linked List/Solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node *insert(Node *head, int data) {
+        if (head == NULL) head = new Node(data);
+        else {
+            Node *curr = head;
+            while (curr->next) curr = curr->next;
+            curr->next = new Node(data);
+        }
+        return head;
+    }
+
+    void display(Node *head) {
+        Node *start = head;
+        while (start) {
+            cout << start->data << " ";
+            start = start->next;
+        }
+    }
+};
+//Aditya Seth
+int main() {
+    Node *head = NULL;
+    Solution mylist;
+    int T, data;
+    cin >> T;
+    while (T-- > 0) {
+        cin >> data;
+        head = mylist.insert(head, data);
+    }
+    mylist.display(head);
+}
