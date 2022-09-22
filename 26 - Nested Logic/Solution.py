39cdfe1ce979d46ac75ef147e2426187258e6e85
Aditya Seth <adityaseth.cse@gmail.com>
2022-09-22 23:20:59 +0530

    Add files via upload

---
+++ 26 - Nested Logic/Solution.py
@@ -0,0 +1,18 @@
+actually = list(map(int, input().split()))
+da, ma, ya = actually
+
+expected = list(map(int, input().split()))
+de, me, ye = expected
+
+fine = 0
+
+if ya > ye:
+    fine = 10000
+elif ya == ye:
+    if ma > me:
+        fine = (ma - me) * 500
+    elif ma == me and da > de:
+        fine = (da - de) * 15
+
+#Aditya Seth
+print(fine)
---
+++ 27- Testing/Solution.cpp
@@ -0,0 +1,55 @@
+#include <stdexcept>
+#include <cassert>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int minimum_index(vector<int> seq) {
+	if (seq.empty()) {
+		throw invalid_argument("Cannot get the minimum value index from an empty sequence");
+	}
+	int min_idx = 0;
+	for (int i = 1; i < seq.size(); ++i) {
+		if (seq[i] < seq[min_idx]) {
+			min_idx = i;
+		}
+	}
+	return min_idx;
+}
+//Aditya Seth
+class TestDataEmptyArray {
+public:
+	static vector<int> get_array() {
+		vector<int> vect{};
+		return vect;
+	}
+
+};
+
+class TestDataUniqueValues {
+public:
+	static vector<int> get_array() {
+		vector<int> vect{ 23,5,8,12,7 };
+		return vect;
+	}
+
+	static int get_expected_result() {
+		return 1;
+	}
+
+};
+
+class TestDataExactlyTwoDifferentMinimums {
+public:
+	static vector<int> get_array() {
+		vector<int> vect{ 9,23,3,8,12,3,7 };
+		return vect;
+	}
+
+	static int get_expected_result() {
+		return 2;
+	}
+
+};
