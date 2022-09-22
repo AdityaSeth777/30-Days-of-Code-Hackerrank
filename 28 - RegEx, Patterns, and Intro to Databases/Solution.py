3e8a9b41cd7c34560a01c77c99b6fba2ad73a1f1
Aditya Seth <adityaseth.cse@gmail.com>
2022-09-22 23:23:51 +0530

    Add files via upload

---
+++ 28 - RegEx, Patterns, and Intro to Databases/Solution.py
@@ -0,0 +1,18 @@
+import re
+
+arr = []
+
+n = int(input())
+
+for i in range(n):
+    data = str(input()).split(" ")
+    name = data[0]
+    email = data[1]
+
+    if re.search(".+@gmail\.com$", email):
+        arr.append(name)
+
+arr.sort()
+#Aditya Seth
+for name in arr:
+    print(name)
