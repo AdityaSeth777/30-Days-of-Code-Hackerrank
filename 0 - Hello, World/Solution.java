0f1f2876bf2a27f8cf902fc9ee264add6a34dc9c
Satyaki Dey <118485043+SatyakiDey75@users.noreply.github.com>
2023-01-09 20:48:37 +0530

    Solution of Hello World question in Java

---
+++ 0 - Hello, World/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any text:");
+        String input=sc.nextLine();
+        System.out.println("Hello, World. \n"+input);
+        sc.close();
+    }
+}
